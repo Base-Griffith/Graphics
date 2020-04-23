272c14a06635320d0df7956bbd62535582052fef
Rupanshu Soi <f20180294@hyderabad.bits-pilani.ac.in>
2020-04-24 00:28:49 +0530

    more refactoring
    Esc will now close window

--- Bezier/bezier.cpp
+++ Bezier/bezier.cpp
@@ -1,4 +1,4 @@
-#include <GL/glut.h>
+#include <GL/freeglut.h>
 #include <iostream>
 #include <vector>
 #include <math.h>
@@ -8,122 +8,47 @@
 #include <windows.h>
 #endif
 
+///////////////////////////////////////////////////////////////////////////////// Global variables to capture program state
+///////////////////////////////////////////////////////////////////////////////
+int degree = -1;
+int radius = 5;
+int currIndex = -1;
+bool isMouseDragging = false;
+
 struct Point
 {
     double x, y;
 };
 
 std::vector<struct Point> points;
-
-void drawBezier();
-int checkAnchor(int x, int y);
-
-///////////////////////////////////////////////////////////////////////////////// Global variables to capture program state
-///////////////////////////////////////////////////////////////////////////////
-int degree = -1;
-int radius = 5;
-int currIndex = -1;
-bool dragging = false;
-///////////////////////////////////////////////////////////////////////////////
-
-
-///////////////////////////////////////////////////////////////////////////////// GLUT Callback Functions
-///////////////////////////////////////////////////////////////////////////////
-void display()
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-    glLoadIdentity();
-
-    // TODO: change to glutSwapBuffers()
-    glFlush();
-}
-
-void reshape(int w, int h)
-{
-    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluOrtho2D(-400, 400, -400, 400);
-    glMatrixMode(GL_MODELVIEW);
-}
-
-void mouseFunc(int button, int state, int x, int y)
-{
-    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN &&
-       (checkAnchor(x - 400 , 400 - y) >= 0))
-    {        
-        currIndex = checkAnchor(x - 400, 400 - y);
-        std::cout << "Found anchor at (" << x << ", " << y << ")" << std::endl;
-        dragging = (state == GLUT_DOWN);
-    }
-
-    else if (button == GLUT_LEFT_BUTTON && state == GLUT_UP &&
-            (checkAnchor(x - 400, 400 - y) < 0))
-    {
-        degree++;
-
-        struct Point p = {x - 400.0f, 400.0f - y};
-        points.push_back(p);
-
-        glBegin(GL_POINTS);
-          glVertex2f(p.x, p.y);
-        glEnd();
-        glFlush();
-
-        drawBezier();
-    }
-
-    else if (button == GLUT_RIGHT_BUTTON && state == GLUT_UP &&
-            (checkAnchor(x - 400, 400 - y) >= 0))
-    {
-        int idx = checkAnchor(x - 400, 400 - y);
-        points.erase(points.begin() + idx);
-
-        degree--;
-
-        drawBezier();
-    }
-}
-
-void motionFunc(int x, int y)
-{
-    if (dragging)
-    {
-        points[currIndex].x = x - 400;
-        points[currIndex].y = 400 - y;
-
-        drawBezier();
-    }
-}
 ///////////////////////////////////////////////////////////////////////////////
 
 
 ///////////////////////////////////////////////////////////////////////////////// Math Utility Functions
 //////////////////////////////////////////////////////////////////////////////
-int factorial(int n)
+long long int factorial(int n)
 {
-    int fact = 1;
-    for(int i = 2; i <= n; i++)
+    long long int fact = 1;
+    for (int i = 2; i <= n; i++)
         fact *= i;
     return fact;
 }
 
-float binomialCoeff(int n, int i, float t)
+double binomialCoeff(int n, int i, double t)
 {
-    float nCi = factorial(n) / (factorial(n-i) * factorial(i));
-    float value = nCi * pow(1 - t, n - i) * pow(t, i);
+    double nCi = factorial(n) / (factorial(n-i) * factorial(i));
+    double value = nCi * pow(1 - t, n - i) * pow(t, i);
     return value;
 }
 
 int checkAnchor(int x, int y)
 {
-    for (int i = 0; i <= degree ; ++i)
+    for (int i = 0; i <= degree; ++i)
     {
-        if( (pow(x-points[i].x,2)+pow(y-points[i].y,2)) <= pow(radius,2)) // check It lies within the circle of anchor
-        {
+	// Check if the point lies within anchor circle
+        double dist = pow(x - points[i].x, 2) + pow(y - points[i].y, 2);
+        if(dist <= radius * radius)
             return i;
-            break;
-        }
     }
     return -1;
 }
@@ -138,50 +63,124 @@ void drawBezier()
     struct Point temp = {0.0f, 0.0f};
 
     glPointSize(1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
 
     // Marking anchor points of the curve
     for (int i = 0; i <= degree; ++i)
         bres_circle(points[i].x, points[i].y, radius);
 
     glBegin(GL_POINTS);
-        for (float t = 0; t <= 1; t += 0.0001)
+        for (double t = 0; t <= 1; t += 0.0001)
         {
-            temp.x=0.0;
-            temp.y=0.0;
+            temp.x = 0.0f;
+            temp.y = 0.0f;
     
             for (int i = 0; i <= degree; ++i)
             {
-                float multFactor = binomialCoeff(degree, i, t);
+                double multFactor = binomialCoeff(degree, i, t);
                 temp.x += multFactor * points[i].x;
                 temp.y += multFactor * points[i].y;
             }
             glVertex2f(temp.x, temp.y);
         }
     glEnd();
-    glFlush();
 }
 ///////////////////////////////////////////////////////////////////////////////
 
 
-void init()
+///////////////////////////////////////////////////////////////////////////////// GLUT Callback Functions
+///////////////////////////////////////////////////////////////////////////////
+void display()
 {
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    drawBezier();
+
+    glutSwapBuffers();
+}
+
+// Closes window on hitting Esc
+void keyboard(unsigned char key, int x, int y)
+{
+    if (key == 27)
+        glutLeaveMainLoop();
+}
+
+void reshape(int w, int h)
+{
+    glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-400, 400, -400, 400);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+void mouseFunc(int button, int state, int x, int y)
+{
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN &&
+       (checkAnchor(x - 400, 400 - y) >= 0))
+    {        
+        currIndex = checkAnchor(x - 400, 400 - y);
+        std::cout << "Found anchor at (" << x << ", " << y << ")" << std::endl;
+        isMouseDragging = (state == GLUT_DOWN);
+    }
+
+    else if (button == GLUT_LEFT_BUTTON && state == GLUT_UP &&
+            (checkAnchor(x - 400, 400 - y) < 0))
+    {
+        degree++;
+
+        struct Point p = {x - 400.0f, 400.0f - y};
+        points.push_back(p);
+
+        glBegin(GL_POINTS);
+          glVertex2f(p.x, p.y);
+        glEnd();
+
+        glutPostRedisplay();
+    }
+
+    else if (button == GLUT_RIGHT_BUTTON && state == GLUT_UP &&
+            (checkAnchor(x - 400, 400 - y) >= 0))
+    {
+        int idx = checkAnchor(x - 400, 400 - y);
+        points.erase(points.begin() + idx);
+
+        degree--;
+
+        glutPostRedisplay();
+    }
+}
+
+void motionFunc(int x, int y)
+{
+    if (isMouseDragging)
+    {
+        points[currIndex].x = x - 400;
+        points[currIndex].y = 400 - y;
+
+        glutPostRedisplay();
+    }
+}
+///////////////////////////////////////////////////////////////////////////////
+
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_RGB);
     glutInitWindowPosition(400,0);
     glutInitWindowSize(800,800);
     glutCreateWindow("Bezier Curve");
 
+    // Registering GLUT callbacks
     glutDisplayFunc(display);
+    glutKeyboardFunc(keyboard);
     glutReshapeFunc(reshape);
     glutMouseFunc(mouseFunc);
     glutMotionFunc(motionFunc);
-}
-
-int main(int argc, char ** argv)
-{
-	glutInit(&argc,argv);
-	init();
-	glutMainLoop();
-	return 0;
+    
+    glutMainLoop();
+    return 0;
 }
 
