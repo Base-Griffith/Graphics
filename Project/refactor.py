8b2106e51c9798d1760b7bd402d5360fb90880c2
Rupanshu Soi <f20180294@hyderabad.bits-pilani.ac.in>
2020-05-04 02:29:51 +0530

    added multiple shading patterns

--- Project/refactor.py
+++ Project/refactor.py
@@ -1,9 +1,19 @@
 import tkinter as tk
 from PIL import Image, ImageTk
+from random import random
 from colors import colors
 
 image = Image.open('smiley.png')
 pixels = image.load()
+
+vis = []
+VISITED = 1
+UNVISITED = 0
+for i in range(image.size[1]):
+  tmp = []
+  for j in range(image.size[0]):
+    tmp.append(UNVISITED)
+  vis.append(tmp)
 
 window = tk.Tk()
 window.geometry(str(image.size[0]) + 'x' + str(image.size[1]))
@@ -22,23 +32,53 @@ agenda = {UP : uplist, DOWN : downlist}
 
 origin = (0, 0)
 interior_color = (0, 0, 0, 0)
-color = colors['green']
+color = colors['orange']
 
-# Note: Y axis is inverted.
+# Note: Y axis is inverted on the screen.
 vdir = UP
+
+def simple_fill(point):
+  pixels[point] = color
+
+def random_fill(point):
+  if random() < 0.1:
+    pixels[point] = color 
+
+def vertical_fill(point):
+  # Set density of lines below
+  if point[0] % 7 == 0:
+    pixels[point] = color
+
+def horizontal_fill(point):
+  # Set density of lines below
+  if point[1] % 9 == 0:
+    pixels[point] = color
+
+def cross_hatch(point):
+  vertical_fill(point)
+  horizontal_fill(point)
+
+patterns = {'fill' : simple_fill,
+            'random' : random_fill,
+            'vertical' : vertical_fill,
+            'horizontal' : horizontal_fill,
+            'cross' : cross_hatch,
+}
 
 def shade_horizontally(point):
   x, y = point[0], point[1]
 
   dx = 1
   while pixels[x + dx, y] == interior_color:
-    pixels[x + dx, y] = color
+    patterns['cross']((x + dx, y))
+    vis[x + dx][y] = VISITED
     dx += 1
   rightb = (x + dx, y)
 
   dx = 0
   while pixels[x + dx, y] == interior_color:
-    pixels[x + dx, y] = color
+    patterns['cross']((x + dx, y))
+    vis[x + dx][y] = VISITED
     dx -= 1 
   leftb = (x + dx, y)
 
@@ -49,32 +89,34 @@ def find_turns(point, leftb, rightb):
   i = leftb[0] + 1
   y = leftb[1] + vdir
   while i < rightb[0]:
-    if pixels[i, y] == interior_color:
+    if pixels[i, y] == interior_color and vis[i][y] != VISITED:
       agenda[vdir].append((i, y))
-      while pixels[i, y] == interior_color:
+      while pixels[i, y] == interior_color and vis[i][y] != VISITED:
         i += 1
     else:
       i += 1
     
+  S_turns_last = i 
+
   # S inverted turns
   i = leftb[0] + 1
   y = leftb[1] - vdir
   while i < rightb[0]:
-    if pixels[i, y] == interior_color:
+    if pixels[i, y] == interior_color and vis[i][y] != VISITED:
       agenda[-1 * vdir].append((i, y))
-      while pixels[i, y] == interior_color:
+      while pixels[i, y] == interior_color and vis[i][y] != VISITED:
         i += 1
     else:
       i += 1
     
   # U turns
-  S_turns_last = i 
+  S_turns_last = max(S_turns_last, i)
   j = rightb[0] + 1
   y = rightb[1]
   while j < S_turns_last:
-    if pixels[j, y] == interior_color:
+    if pixels[j, y] == interior_color and vis[j][y] != VISITED:
       agenda[-1 * vdir].append((j, y))
-      while pixels[j, y] == interior_color:
+      while pixels[j, y] == interior_color and vis[j][y] != VISITED:
         j += 1
     else:
       j += 1
